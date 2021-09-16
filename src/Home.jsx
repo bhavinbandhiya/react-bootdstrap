54ac2bdd88b7efa7eeb2da2c52cf7a716dbc1182
bhavinbandhiya <bhavinbandhiya003@gmail.com>
2021-09-16 15:26:33 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <section id="header" className="">
+        <div className="container-fluid nav_bg">
+          <div className="row">
+            <div className="col-10 mx-auto">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                    <h1>Grow Your business with <strong className="brand-name">Bhavin Bandhiya</strong></h1>
+                    <h2 className="my-3">
+                        we are the team of talented devloper
+                    </h2>
+                        <div className="mt-3">
+                            <a href="" className="btn btn-primary">Get Started</a>
+                        </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
